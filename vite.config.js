df7cf49952a6a5514b3fe7992ed7214e0c1e7ca5
rishabhxcode <rishabhxcode@gmail.com>
2024-10-29 23:56:47 +0530

    restructure

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import restart from "vite-plugin-restart";
 
 export default {
-  root: "src/", // Sources files (typically where index.html is)
-  publicDir: "../static/", // Path from "root" to static assets (files that are served as they are)
+  root: ".", // Sources files (typically where index.html is)
+  publicDir: "./static/", // Path from "root" to static assets (files that are served as they are)
   server: {
     host: true, // Open to local network and display URL
     open: !("SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env), // Open if it's not a CodeSandbox

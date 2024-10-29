8a353e9417e54b342e076a6bcfa6cb4e3560fbcf
rishabhxcode <rishabhxcode@gmail.com>
2024-10-30 00:16:04 +0530

    paths re-worked

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-import * as THREE from "/node_modules/three/build/three.module.js";
-import { OrbitControls } from "/node_modules/three/examples/jsm/controls/OrbitControls.js";
-import { FontLoader } from "/node_modules/three/examples/jsm/loaders/FontLoader.js";
-import { TextGeometry } from "/node_modules/three/examples/jsm/geometries/TextGeometry.js";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
 
 const sizes = {
   width: window.innerWidth,
